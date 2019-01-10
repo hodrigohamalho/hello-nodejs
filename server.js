bcf5a23776d388a437f1dc7547409f65c0ec7826
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2019-01-10 02:05:27 -0200

    fixing hello message

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 // app is running!
 app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date());
+    res.send('<h1> Hello Openshift - NodeJS! </h1>');
 });
 
 app.listen(8080, ip);
